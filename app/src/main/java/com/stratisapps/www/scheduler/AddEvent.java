49df4e24c0463f088377b1b2061eae0cc843b706
AlexanderLHS <alexanderarreola2019@gmail.com>
2019-03-15 05:40:04 -0700

    Updated 3/15/19

--- app/src/main/java/com/stratisapps/www/scheduler/AddEvent.java
+++ app/src/main/java/com/stratisapps/www/scheduler/AddEvent.java
@@ -16,6 +16,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -41,6 +42,7 @@ public class AddEvent extends AppCompatActivity implements DatePickerDialog.OnDa
     private DatePickerDialog datePickerDialog = null;
     private TimePickerDialog timePickerDialog = null;
     private Button timeFormat = null;
+    private ScrollView scrollView = null;
     private boolean passedFormatTest = true;
     private Calendar calendar = Calendar.getInstance();
     private SharedPreferences sharedPreferences = null;
@@ -64,6 +66,7 @@ public class AddEvent extends AppCompatActivity implements DatePickerDialog.OnDa
         time = findViewById(R.id.time);
         timeBox = findViewById(R.id.timeBox);
         timeFormat = findViewById(R.id.timeFormat);
+        scrollView = findViewById(R.id.scroll);
         datePickerDialog = new DatePickerDialog(this, R.style.DialogColor,this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
         timePickerDialog = new TimePickerDialog(this, R.style.DialogColor,this, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true);
         fillDateBox(calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.YEAR));
@@ -122,6 +125,7 @@ public class AddEvent extends AppCompatActivity implements DatePickerDialog.OnDa
                     time.setHint(null);
                 }
                 else{
+                    scrollView.scrollBy(0,50);
                     time.setHint("hh:mm");
                 }
             }
@@ -167,10 +171,10 @@ public class AddEvent extends AppCompatActivity implements DatePickerDialog.OnDa
                     passedAllTests = false;
                 }
                 else {
-                    SimpleDateFormat originalFormat = new SimpleDateFormat("hh:mm");
+                    SimpleDateFormat originalFormat = new SimpleDateFormat("hh:mm a");
                     Date test = null;
                     try{
-                        test = originalFormat.parse(timeString);
+                        test = originalFormat.parse(timeString + " " + timeFormat.getText().toString().toLowerCase());
                         passedFormatTest = true;
                     }
                     catch (Exception e ){
@@ -180,7 +184,12 @@ public class AddEvent extends AppCompatActivity implements DatePickerDialog.OnDa
                             Integer.valueOf(timeString.substring(0, timeString.indexOf(":"))) <= 12 && Integer.valueOf(timeString.substring(timeString.indexOf(":") + 1, timeString.length())) < 60){
                         Date currentDate = null;
                         try {
-                            currentDate = originalFormat.parse(String.valueOf(calendar.get(Calendar.HOUR_OF_DAY)) + ":" + String.valueOf(calendar.get(Calendar.MINUTE)));
+                            if(calendar.get(Calendar.AM_PM) == 0){
+                                currentDate = originalFormat.parse(String.valueOf(calendar.get(Calendar.HOUR_OF_DAY)) + ":" + String.valueOf(calendar.get(Calendar.MINUTE)) + " am");
+                            }
+                            else {
+                                currentDate = originalFormat.parse(String.valueOf(calendar.get(Calendar.HOUR_OF_DAY)) + ":" + String.valueOf(calendar.get(Calendar.MINUTE)) + " pm");
+                            }
                         } catch (ParseException e) {
                             e.printStackTrace();
                         }
